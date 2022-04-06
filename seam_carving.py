6b42dd23336f3a1eea788a1800ebcaa56510931a
dfelig <felig@mail.tau.ac.il>
2022-04-06 14:22:57 +0300

    Created branch

--- seam_carving.py
+++ seam_carving.py
@@ -1,9 +1,30 @@
-from typing import Dict, Any
+from copy import deepcopy
+from typing import Dict, Any, List, Tuple
 
 import numpy as np
 
+from utils import get_gradients, to_grayscale
+
 NDArray = Any
 
+
+class Seam:
+    def __init__(self, pixels: NDArray):
+        self.pixels = pixels
+
+
+
+class Pixel:
+    def __init__(self, i: int, j: int):
+        self.indices = np.array([i,j])
+
+
+# pixel_energy_matrix = None
+# index_mapping_matrix = None
+# vertical_seams = None
+# horizontal_seams = None
+# vertical_seams_to_find = -1
+# horizontal_seams_to_find = -1
 
 def resize(image: NDArray, out_height: int, out_width: int, forward_implementation: bool) -> Dict[str, NDArray]:
     """
@@ -18,16 +39,38 @@ def resize(image: NDArray, out_height: int, out_width: int, forward_implementati
             where img1 is the resized image and img2/img3 are the visualization images
             (where the chosen seams are colored red and black for vertical and horizontal seams, respecitvely).
     """
-    raise NotImplementedError('You need to implement this!')
+
+    def calculate_cost_matrix() -> NDArray:
+        pixel_energy_matrix
+
+def find_optimal_seam(cost_matrix: NDArray) -> Seam:
+    pixels = []
+    last_column_index = cost_matrix.shape[0] - 1
+    row_index = np.argmin(cost_matrix[last_column_index])
+    pixels.append((last_column_index, row_index))
+    for i in range(last_column_index):
+        column_index = last_column_index - 1
+        if cost_matrix[column_index, row_index] == pixel_energy_matrix[column_index, row_index] + cost_matrix[
+            column_index - 1, row_index] + c_left(column_index, row_index)
+
+    return Seam(pixels)
+
+    pixel_energy_matrix = get_gradients(deepcopy(image))
+    vertical_seams_to_find = abs(out_width - image.shape[1])
+    horizontal_seams_to_find = abs(out_width - image.shape[0])
+    # initialize_pixel_energy_matrix()
+    # initialize_index_mapping_matrix(dimensions)
+    current_image = to_grayscale(image)
+
+    for i in range(vertical_seams_to_find):
+        cost_matrix = calculate_cost_matrix()
+        seam = find_optimal_seam(cost_matrix)
+        remove_seam(seam, current_image) # remove from image, add seam to list, shift index matrix
+
+    # raise NotImplementedError('You need to implement this!')
     # TODO: return { 'resized' : img1, 'vertical_seams' : img2 ,'horizontal_seams' : img3}
 
-# TODO:
-#       get_matrix_cost - Ben
-#             - given energy function
-#             - forward looking function
-#       find_optimal_seam (cost_matrix) - Daniella
-#       shift_img_left (seam_to_remove)
-#       shift_img_right (seam_to_duplicate)
-#       create_original_indices_matrix (original_img)
-#       get_original_index (pixel, original_indices_matrix)
-#       collect_seam (collected_seams)
+
+
+
+
